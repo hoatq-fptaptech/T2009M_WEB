b4f9393f96d4c85b68d70729bd8acd01f4791d9e
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-11-03 11:54:55 +0700

    lab s12 - buoc 2

--- js/grid.js
+++ js/grid.js
@@ -50,6 +50,22 @@ var products=[
 ]
 function gridProduct() {
     var grid_html = "";
-    // viet giao dienj tra ve
+    for(var i=0;i<6;i++){
+        grid_html+= "<div class=\"col-md-4\">\n" +
+            "                     <div class=\"item text-center\">\n" +
+            "                         <img class=\"product-image\" src=\"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg\" height=\"700\" width=\"700\"/>\n" +
+            "                         <h2 class=\"product-name\">Flying ninja</h2>\n" +
+            "                         <div class=\"star\">\n" +
+            "                             <div class=\"star-box\">\n" +
+            "                                 <div class=\"star-rate\" style=\"width: 74%\"></div>\n" +
+            "                             </div>\n" +
+            "                         </div>\n" +
+            "                         <p class=\"price\">$12</p>\n" +
+            "                         <a href=\"#\" class=\"add-to-cart text-uppercase\">Add To Cart</a>\n" +
+            "                     </div>\n" +
+            "                 </div>";
+    }
     return grid_html;
 }
+var products_html = document.getElementById("products");
+products_html.innerHTML = gridProduct();
