b2a4f022cc2c834f950a273c8dd4a48d0df92d7a
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-11-03 12:17:33 +0700

    lab s12 - buoc 3

--- js/grid.js
+++ js/grid.js
@@ -2,65 +2,78 @@ var products=[
     {
         productName: "Flying ninja",
         price: 12,
-        image:"/imgs/img1.png",
+        image:"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg",
+        star:5
+    },
+    {
+        productName: "Flying ninja",
+        price: 12,
+        image:"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg",
+        star:4
+    },
+    {
+        productName: "Flying ninja",
+        price: 12,
+        image:"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg",
         star:3.5
     },
     {
         productName: "Flying ninja",
         price: 12,
-        image:"/imgs/img1.png",
+        image:"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg",
+        star:3
+    },
+    {
+        productName: "Flying ninja",
+        price: 12,
+        image:"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg",
+        star:1
+    },
+    {
+        productName: "Flying ninja",
+        price: 12,
+        image:"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg",
+        star:2
+    },
+    {
+        productName: "Flying ninja",
+        price: 12,
+        image:"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg",
+        star:0
+    },
+    {
+        productName: "Flying ninja",
+        price: 12,
+        image:"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg",
         star:3.5
     },
     {
         productName: "Flying ninja",
         price: 12,
-        image:"/imgs/img1.png",
+        image:"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg",
         star:3.5
     },
     {
         productName: "Flying ninja",
         price: 12,
-        image:"/imgs/img1.png",
-        star:3.5
-    },
-    {
-        productName: "Flying ninja",
-        price: 12,
-        image:"/imgs/img1.png",
-        star:3.5
-    },
-    {
-        productName: "Flying ninja",
-        price: 12,
-        image:"/imgs/img1.png",
-        star:3.5
-    },
-    {
-        productName: "Flying ninja",
-        price: 12,
-        image:"/imgs/img1.png",
-        star:3.5
-    },
-    {
-        productName: "Flying ninja",
-        price: 12,
-        image:"/imgs/img1.png",
+        image:"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg",
         star:3.5
     }
+
 ]
 function gridProduct() {
     var grid_html = "";
-    for(var i=0;i<6;i++){
+    for(var i=0;i<products.length;i++){
         grid_html+= "<div class=\"col-md-4\">\n" +
             "                     <div class=\"item text-center\">\n" +
-            "                         <img class=\"product-image\" src=\"imgs/ao-thun-tennis-tts100-cho-nam-den.jpg\" height=\"700\" width=\"700\"/>\n" +
-            "                         <h2 class=\"product-name\">Flying ninja</h2>\n" +
+            "                         <img class=\"product-image\" src=\""+products[i].image+"\" height=\"700\" width=\"700\"/>\n" +
+            "                         <h2 class=\"product-name\">"+products[i].productName+"</h2>\n" +
             "                         <div class=\"star\">\n" +
             "                             <div class=\"star-box\">\n" +
-            "                                 <div class=\"star-rate\" style=\"width: 74%\"></div>\n" +
+            "                                 <div class=\"star-rate\" style=\"width:"+(products[i].star/5*100)+"%\"></div>\n" +
             "                             </div>\n" +
             "                         </div>\n" +
-            "                         <p class=\"price\">$12</p>\n" +
+            "                         <p class=\"price\">$"+products[i].price+"</p>\n" +
             "                         <a href=\"#\" class=\"add-to-cart text-uppercase\">Add To Cart</a>\n" +
             "                     </div>\n" +
             "                 </div>";
